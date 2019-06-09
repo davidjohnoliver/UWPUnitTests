ccf89417d34acff79407145b621b6b6db9066861
David Oliver <david.oliver@nventive.com>
2019-06-09 13:46:46 -0400

    Set up projects

---
+++ UWPUnitTests2.Net461/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UWPUnitTests2.Net461
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
