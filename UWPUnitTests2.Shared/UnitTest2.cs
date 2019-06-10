f86ba1a4670fe38437386029afc255858bb9ad1d
David Oliver <david.oliver@nventive.com>
2019-06-10 10:25:02 -0400

    Add Given_Grid_And_Min_Max

---
+++ UWPUnitTests2.Shared/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UWPUnitTests2
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			;
+		}
+	}
+}
