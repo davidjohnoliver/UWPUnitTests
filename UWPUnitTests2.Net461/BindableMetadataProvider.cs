ec5618151d2f6bd2a008fda1e1e3688109d96776
David Oliver <david.oliver@nventive.com>
2019-09-16 15:09:51 -0400

    Use App from Uno.UI.Tests

---
+++ UWPUnitTests2.Net461/BindableMetadataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.UI.DataBinding;
+
+namespace UWPUnitTests2.Net461
+{
+    public class BindableMetadataProvider : IBindableMetadataProvider
+    {
+        public IBindableType GetBindableTypeByFullName(string fullName)
+        {
+            return null;
+        }
+
+        public IBindableType GetBindableTypeByType(Type type)
+        {
+            return null;
+        }
+    }
+}
---
+++ UWPUnitTests2/App.xaml.tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Uno.UI.Tests.App.Xaml;
+using UWPUnitTests2;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UnitTestsApp
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	sealed partial class App : Application
+	{
+		internal const bool EnableInteractiveTests = false;
+
+		async partial void OnLaunchedPartial()
+		{
+			await Task.Delay(50);
+
+			TestRunner.Run();
+
+			await Task.Run(async () =>
+			{
+				await Task.Delay(1500); //Puts the message after the symbol-loading spam
+				Debug.WriteLine("Tests have completed.");
+			});
+		}
+	}
+}
